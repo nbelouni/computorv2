1966ef9c926067477782964332e60f79fdac8925
Nawel BELOUNIS <nbelouni@e1r12p7.42.fr>
2019-12-05 17:01:19 +0100

    includ

---
+++ inc/Computorv2.hpp
@@ -0,0 +1,13 @@
+#ifndef COMPUTOR_V2_HPP
+# define COMPUTOR_V2_HPP
+
+#include <iostream>
+
+#include "Operand.hpp"
+//#include "Complex.hpp"
+//#include "LexerParser.hpp"
+#include "Real.hpp"
+#include "Computor.hpp"
+//#include "Matrix.hpp"
+
+#endif
